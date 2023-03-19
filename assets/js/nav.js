64b46316df0908761dea57f707e1cd955bbeaae6
Morteza <mortazapor3@gmail.com>
2023-03-19 23:26:06 +0330

    Refactore .scss files  and Add some mixins

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,6 +5,7 @@ let headerSocial = $.querySelector('.header__social');
 
 function fixNavBar() {
     let scrollCondition = window.scrollY >= navTopElem.scrollHeight;
+
     navTopElem.style.cssText = scrollCondition ? 'z-index: 1; position: fixed; top: 0; right: 0; left: 0;border-radius: 0; padding: 1rem' : '';
     headerSocial.style.display = scrollCondition ? 'none' : '';
     navBottomElem.style.display = scrollCondition ? 'none' : '';
--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,15 +1,1 @@
 let $ = document;
-let headerCategorySelector = $.querySelector('.search-input__category-selector');
-let headerCategoryList = $.querySelector('.category-selector__list');
-
-function showHideCategoryList() {
-    let checkCategoryListState = headerCategoryList.classList.contains('hidden');
-
-    if (checkCategoryListState) {
-        headerCategoryList.classList.replace('hidden', 'show-block');
-    } else {
-        headerCategoryList.classList.replace('show-block', 'hidden');
-    }
-}
-
-headerCategorySelector.addEventListener('click', showHideCategoryList);
