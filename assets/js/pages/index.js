fa7eb7c1307ae10b3cff944898258660a84cc77d
Morteza <mortazapor3@gmail.com>
2023-03-23 22:47:38 +0330

    Complete footer html codes

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -39,3 +39,31 @@ function hidePopUp(e) {
     popUpElem.style.display = 'none';
 }
 
+// Posts and Products Slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+        el: '.my-own-pagination',
+        clickable: true,
+        bulletClass: "bullet",
+        bulletActiveClass: "active-bullet",
+        type: 'bullets',
+    },
+});
+
+const scrollBarSlider = new Swiper('.scrollbar-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 4,
+    spaceBetween: 20,
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        enabled: false,
+    }
+});
