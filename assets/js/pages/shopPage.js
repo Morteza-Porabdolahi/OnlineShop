d6c97429cf0539d67cea708809c07ff200a733d7
Morteza <mortazapor3@gmail.com>
2023-03-31 11:44:52 +0330

    Complete shop page generay styling and Complete a part of single product page styling

---
+++ assets/js/pages/shopPage.js
@@ -0,0 +1,40 @@
+const $ = document;
+
+const minRangeInput = $.querySelector('.min-range');
+const maxRangeInput = $.querySelector('.max-range');
+const rangeStartPrice = $.querySelector('.start__price');
+const rangeEndPrice = $.querySelector('.end__price');
+const rangeInputsProgress = $.querySelector('.progress');
+
+let maxPrice = 599000;
+
+minRangeInput.addEventListener('input', handleMinRangeInput);
+maxRangeInput.addEventListener('input', handleMaxRangeInput);
+
+function handleMaxRangeInput() {
+    maxRangeInput.min = minRangeInput.value;
+    maxRangeInput.style.width = `${(((maxPrice - minRangeInput.value) / maxPrice) * 100) + 5}%`;
+
+    rangeEndPrice.textContent = maxRangeInput.value;
+
+    handleProgressWidth();
+
+    minRangeInput.classList.remove('range-input--active');
+}
+
+function handleMinRangeInput() {
+    minRangeInput.max = maxRangeInput.value;
+    minRangeInput.style.width = `${(maxRangeInput.value / maxPrice) * 100}%`;
+
+    rangeStartPrice.textContent = minRangeInput.value;
+
+    handleProgressWidth();
+
+    minRangeInput.classList.add('range-input--active');
+}
+
+function handleProgressWidth() {
+    rangeInputsProgress.style.right = `${(minRangeInput.value / maxPrice) * 100}%`
+    rangeInputsProgress.style.left = `${((maxPrice - maxRangeInput.value) / maxPrice) * 100}%`
+
+}
